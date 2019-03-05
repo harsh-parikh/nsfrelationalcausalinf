d83498ad7ac944b5a60177b832f6ef429174009b
Harsh Parikh <harsh081193@gmail.com>
2019-03-05 14:36:09 -0500

    Update set_summary_kernel.py

--- set_summary_kernel.py
+++ set_summary_kernel.py
@@ -3,3 +3,13 @@ import numpy as np
 import scipy
 from sklearn.neighbors.kde import KernelDensity
 
+def subset(D,sid,i):
+    Di = 0
+    return Di
+
+def summarize_set(D,sid,summary_col_array):
+    #for each element of sid, form a set and summarize the set using KDE.
+    sidarray = unique(D[:,sid])
+    for i in sidarray:
+        Di = subset(D,sid,i)
+        Xi = subcol(Di,summary_col_array)
