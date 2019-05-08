b049f1d4a76fb30d385e1c7bdb2bba1f8a358e58
Moe Kayali <moe@kayali.io>
2019-05-08 13:55:46 -0700

    Optimizations
    Training variable model down to ~3 minutes.

--- synthetic/variable_size.py
+++ synthetic/variable_size.py
@@ -40,11 +40,11 @@ p_skilled_instructor = 0.5
 p_difficult_course = 0.5
 
 difficulty = np.array([random.random() < p_difficult_course
-                       for _ in courses], dtype=np.uint8).reshape(-1, 1)
+                       for _ in courses], dtype=np.float32).reshape(-1, 1)
 skill = np.array([random.random() < p_skilled_instructor
-                  for _ in instructors], dtype=np.uint8).reshape(-1, 1)
+                  for _ in instructors], dtype=np.float32).reshape(-1, 1)
 intelligence = np.array([random.random() < p_intelligent_student
-                  for _ in students], dtype=np.uint8).reshape(-1, 1)
+                  for _ in students], dtype=np.float32).reshape(-1, 1)
 
 registered = []
 lecturers = []
@@ -123,33 +123,35 @@ class VariableEmbeddingClassifier(nn.Module):
         )
 
     def forward(self, diff, skills, aptitude):
-        agg_skill = torch.FloatTensor([0])
-        for s in skills:
-            agg_skill += self.input_sk(s.unsqueeze(dim=-1))
+        agg_skill = self.input_sk(skills.unsqueeze(dim=-1))\
+            .sum().unsqueeze(dim=-1)
         count = len(skills)
-        temp = torch.cat((agg_skill, torch.FloatTensor([count])), dim=-1)
+        temp = torch.cat((agg_skill, torch.Tensor([count])), dim=-1)
         sk = self.output_sk(temp)
 
-        agg_apt = torch.FloatTensor([0])
-        for apt in aptitude:
-            agg_apt += self.input_i(apt.unsqueeze(dim=-1))
+        agg_apt = self.input_i(aptitude.unsqueeze(dim=-1))\
+            .sum().unsqueeze(dim=-1)
         count = len(skills)
-        temp = torch.cat((agg_apt, torch.FloatTensor([count])), dim=-1)
+        temp = torch.cat((agg_apt, torch.Tensor([count])), dim=-1)
         i = self.output_i(temp)
 
         processed = torch.cat((diff, sk, i), dim=0)
         return self.model(processed)
 
 
-labels_t = torch.FloatTensor(labels)
-embeddings_t = torch.FloatTensor(embeddings)
+labels_t = torch.Tensor(labels)
+embeddings_t = torch.Tensor(embeddings)
 
 pad_registered = pad(registered)
 pad_lecturers = pad(lecturers)
 
-pad_grounding = torch.cat((torch.FloatTensor(difficulty),
-                       torch.FloatTensor(pad_registered),
-                       torch.FloatTensor(pad_lecturers)), dim=1)
+pad_grounding = torch.cat((torch.Tensor(difficulty),
+                       torch.Tensor(pad_registered),
+                       torch.Tensor(pad_lecturers)), dim=1)
+
+registered_t = [torch.Tensor(r) for r in registered]
+difficulty_t = torch.Tensor(difficulty)
+lecturers_t = [torch.Tensor(l) for l in lecturers]
 
 oracle_model = OracleClassifier()
 pad_model = PaddedEmbeddingClassifier()
@@ -186,18 +188,17 @@ for i in range(epochs):
     print(f"loss: {loss:.04}, "
           f"{(pad_output.round() == labels_t).sum()} / 5000 correct, ")
 
+
 print("training variable")
 for i in range(epochs):
-    total_loss = 0
     loss = 0
     var_model.zero_grad()
     var_outputs = []
     for c in courses:
-        var_outputs.append(var_model(torch.FloatTensor(difficulty[c]),
-                                torch.FloatTensor(lecturers[c]),
-                                torch.FloatTensor(registered[c])))
+        var_outputs.append(var_model(difficulty_t[c], lecturers_t[c],
+                                     registered_t[c]))
     var_output = torch.stack(var_outputs)
-    loss += criterion(var_output, labels_t)
+    loss = criterion(var_output, labels_t)
     loss.backward()
     var_optimizer.step()
     print(f"loss: {loss:.04}, "
@@ -215,11 +216,13 @@ plt.plot(fpr, tpr, 'g', label = 'Oracle, AUC = %0.2f' % roc_auc)
 
 fpr, tpr, threshold = roc_curve(labels, pad_output.detach().numpy())
 roc_auc = auc(fpr, tpr)
-plt.plot(fpr, tpr, 'orange', label = 'Padded Embed Learning, AUC = %0.2f' % roc_auc)
+plt.plot(fpr, tpr, 'orange', label = 'Padded Embed Learning, AUC = %0.2f'
+                                     % roc_auc)
 
 fpr, tpr, threshold = roc_curve(labels, var_output.detach().numpy())
 roc_auc = auc(fpr, tpr)
-plt.plot(fpr, tpr, 'b', label = 'Variable Embed Learning, AUC = %0.2f' % roc_auc)
+plt.plot(fpr, tpr, 'b', label = 'Variable Embed Learning, AUC = %0.2f'
+                                % roc_auc)
 
 plt.legend(loc = 'lower right')
 plt.plot([0, 1], [0, 1],'r--')
