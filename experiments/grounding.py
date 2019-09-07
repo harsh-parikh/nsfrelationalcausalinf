c73043d0e63342828af9fa6cb01187c385a82aec
Moe Kayali <moe@kayali.io>
2019-09-07 14:18:51 -0700

    more robust email filtering
    this change allows for filtering domains like “yahoo.fr” or “hotmail.co.uk”.

--- experiments/grounding.py
+++ experiments/grounding.py
@@ -3,16 +3,23 @@
 import json
 from collections import defaultdict
 
+
 def collaboration(paper):
+    '''Decide if paper is a collaboration between multiple institutions.
+
+    Do this by looking at email domains, ignoring commercial providers.
+    '''
+    ignore = {"gmail", "yahoo", "hotmail"}
     domains = set()
 
     for a in paper['authorids']:
         d = a.split("@")[-1]
-        if d == "gmail.com" or d == "yahoo.com":
+        if any(x in d for x in ignore):
             continue
         domains.add(d)
 
     return len(domains) > 1
+
 
 with open("../openreview-dataset/results/papers.json", "r") as f:
     papers = json.load(f)
