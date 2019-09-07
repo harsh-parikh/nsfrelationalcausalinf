61f4afb21fe2a813909d29c37aa7992776fc2c7a
Moe Kayali <moe@kayali.io>
2019-09-07 14:08:15 -0700

    consider effect of collaborations

--- experiments/grounding.py
+++ experiments/grounding.py
@@ -3,6 +3,17 @@
 import json
 from collections import defaultdict
 
+def collaboration(paper):
+    domains = set()
+
+    for a in paper['authorids']:
+        d = a.split("@")[-1]
+        if d == "gmail.com" or d == "yahoo.com":
+            continue
+        domains.add(d)
+
+    return len(domains) > 1
+
 with open("../openreview-dataset/results/papers.json", "r") as f:
     papers = json.load(f)
 
@@ -18,3 +29,7 @@ for r in reviews:
 
     # Accept[P] ⃪ Confidence[P, R] where Reviewed(R, P).
     grounding[("accept", r["paperhash"])].add(("confidence", r["norm_conf"]))
+
+for p in papers:
+    # Accept[P] ⃪ Is_Collab[P].
+    grounding[("accept", p["paperhash"])].add(("is_collab", collaboration(p)))
