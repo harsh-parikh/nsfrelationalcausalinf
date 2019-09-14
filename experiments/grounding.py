0436e0934159ef24e2666553bfc4d93d839f93f2
Harsh Parikh <harsh081193@gmail.com>
2019-09-14 12:30:27 -0400

    minor corrections

--- experiments/grounding.py
+++ experiments/grounding.py
@@ -37,12 +37,13 @@ with open("../openreview-dataset/results/confs.json", "r") as f:
 # maps unit (symbol, value) to set of incoming edges
 grounding = defaultdict(set)
 
-for r in reviews:
+for key,rs in reviews.items():
     # Accept[P] ⃪ Score[P, R] where Reviewed(R, P).
-    grounding[("accept", r)].add(("score", r["norm_rating"]))
-
-    # Accept[P] ⃪ Confidence[P, R] where Reviewed(R, P).
-    grounding[("accept", r)].add(("confidence", r["norm_conf"]))
+    for r in rs:
+        grounding[("accept", key)].add(("score", r["norm_rating"]))
+    
+        # Accept[P] ⃪ Confidence[P, R] where Reviewed(R, P).
+        grounding[("accept", key)].add(("confidence", r["norm_conf"]))
 
 for p in papers:
     # Accept[P] ⃪ Is_Collab[P].
