013b38dcc1cf3fb07261b24b28cd50baeef1247a
Moe Kayali <moe@kayali.io>
2019-09-25 14:30:19 -0700

    some BART stuff

--- experiments/prestige.py
+++ experiments/prestige.py
@@ -2,6 +2,7 @@
 from common import load_dataset, publishing_years, prestigious, h_index
 
 import numpy as np
+from sklearn.model_selection import cross_val_score
 
 all_authors, papers, reviews, confs = load_dataset()
 
@@ -42,3 +43,11 @@ for p in papers:
 
 flat = np.array(flat)
 print(np.mean(flat, axis=0))
+
+from bartpy.sklearnmodel import SklearnModel
+model = SklearnModel() # Use default parameters
+X = flat[:, :-1]
+y = np.ravel(flat[:, -1:])
+
+scores = cross_val_score(model, X, y, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
